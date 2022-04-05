4df4e00afd61f495f579381a48c8e589510c4dca
RichardWindF <romewind1@gmail.com>
2022-04-05 01:31:34 -0400

    form action+method+target and Select

---
+++ WebFrontEnd2022mar/html/html-3form表单/action.php
@@ -0,0 +1,12 @@
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Document</title>
+ </head>
+ <body>
+     这是一个服务器文件，这里只是用php 来代替，从而测试form 表单属性action=url什么的
+ </body>
+ </html>
